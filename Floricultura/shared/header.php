251aca354aa54cfbb67c791ce39d7e26bb7a23db
jo-nunes13 <103642053+jo-nunes13@users.noreply.github.com>
2024-11-30 23:10:55 -0300

    Criação das páginas

---
+++ Floricultura/shared/header.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Floricultura</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="src/css/bootstrap.min.css" rel="stylesheet">
+    <script src="src/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body >
+    <nav class="navbar navbar-expand-sm bg-success ">
+
+        <div class="container-fluid">
+            <!-- Links -->
+            <ul class="navbar-nav float-end">
+            
+                <li class="nav-item ">
+                    <a class="nav-link" href="carrinho.php">Carrinho</a>
+                </li>
+                
+            </ul>
+        </div>
+    </nav>
+
+    <nav class="navbar navbar-expand-md d-flex bg-dark justify-content-center navbar-dark">
+        <div>
+            <!-- Links -->
+            <ul class="navbar-nav">
+            
+                <li class="nav-item">
+                    <a class="nav-link" href="prodJard.php">Jardinagem</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Floricultura</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="blog.php">Blog das Plantas</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid">
+    
+                    <div class="dropdown float-start">
+                        <button type="button" class="btn btn-dark text-white dropdown-toggle" data-bs-toggle="dropdown">
+                            Filtros de pesquisa
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Link 1</a></li>
+                            <li><a class="dropdown-item" href="#">Link 2</a></li>
+                            <li><a class="dropdown-item" href="#">Link 3</a></li>
+                        </ul>
+                    </div>
+                
+    
